a5e1a891a30e92dfac54c1895a124efecf6feab6
Katoole <kta9611@yahoo.co.kr>
2018-06-26 17:43:41 +0900

    Update selfservice.sh

--- selfservice.sh
+++ selfservice.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt install -y mariadb-client-core-10.0 &&
-
-mysql -uroot -proot -h127.0.0.1 database < ./backup/database.sql &&
-
 docker-compose up -d
+
+apt install -y mariadb-client-core-10.0
+
+mysql -uroot -proot -h127.0.0.1 database < ./backup/database.sql
+
