0967e6d29748e93777146dcda70ddf21f6cdf8b5
Katoole <kta9611@yahoo.co.kr>
2018-06-26 17:49:53 +0900

    Update selfservice.sh

--- selfservice.sh
+++ selfservice.sh
@@ -4,5 +4,5 @@ docker-compose up -d
 
 apt install -y mariadb-client-core-10.0
 
-mysql -uroot -proot -h127.0.0.1 database < ./backup/database.sql
+mysql -uroot -proot -h127.0.0.1 database < /METROM/Demo_Wordpress/backup/database.sql
 
